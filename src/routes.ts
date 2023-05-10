75999fe6e0a46b20d31d7fea5007013146a999a4
Luiz Otavio <luizotavio0106@outlook.com>
2023-05-10 13:02:34 -0300

    refactoring the default code and importing the express typing

--- src/routes.ts
+++ src/routes.ts
@@ -1,23 +1,9 @@
-import { UserController } from "./controller/UserController"
+import { Router, Request, Response } from 'express';
 
-export const Routes = [{
-    method: "get",
-    route: "/users",
-    controller: UserController,
-    action: "all"
-}, {
-    method: "get",
-    route: "/users/:id",
-    controller: UserController,
-    action: "one"
-}, {
-    method: "post",
-    route: "/users",
-    controller: UserController,
-    action: "save"
-}, {
-    method: "delete",
-    route: "/users/:id",
-    controller: UserController,
-    action: "remove"
-}]
+const routes = Router();
+
+routes.get('/', (request: Request, response: Response) => {
+  return response.json({ message: 'Hello world' });
+});
+
+export default routes;
