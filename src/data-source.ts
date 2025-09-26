80ebfe8c870b79e55d42bf0ad7cb6583b47a8398
Luiz Otavio <locchavescarvalho@gmail.com>
2025-09-25 22:17:10 -0300

    fix: rename db_port environment variable

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,7 +6,7 @@ require('dotenv').config();
 export const AppDataSource = new DataSource({
   type: 'postgres',
   host: process.env.DB_HOST,
-  port: Number(process.env.PORT) || 5432,
+  port: Number(process.env.DB_PORT) || 5432,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
