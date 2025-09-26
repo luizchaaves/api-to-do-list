f89d53933470e10caf6d9e1559a96ed315e573ad
Luiz Otavio <locchavescarvalho@gmail.com>
2025-09-25 21:44:47 -0300

    fix: update enviroment variables in data source

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,8 +6,8 @@ require('dotenv').config();
 export const AppDataSource = new DataSource({
   type: 'postgres',
   host: process.env.DB_HOST,
-  port: 5432,
-  username: 'db_todo_3ei8_user',
+  port: Number(process.env.PORT) || 5432,
+  username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   synchronize: true,
